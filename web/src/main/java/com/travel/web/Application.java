acab8ce8f30500c79f310be494ddd90343628e00
guochao.song <guochao.song@dmall.com>
2020-05-05 22:23:01 +0800

    init

---
+++ web/src/main/java/com/travel/web/Application.java
@@ -0,0 +1,29 @@
+package com.travel.web;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.travel")
+@EnableCaching
+public class Application implements CommandLineRunner {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String encoding = System.getProperty("file.encoding");
+        LOGGER.info("current system file.encoding:" + encoding);
+    }
+}
+
